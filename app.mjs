1263ca5483f7d6549fa5107bd47c3410c9e3fd16
Seba Vidal <sebastian.f.vidal@gmail.com>
2023-02-13 14:06:02 -0300

    primer commit del servidor

---
+++ app.mjs
@@ -0,0 +1,52 @@
+import express from 'express'; 
+const app = express(); //retorna aplicacion de express. 
+import http from 'http'; 
+import {randoms} from './datos.mjs'; 
+
+app.use(express.json()); 
+
+//Routing
+app.get('/', (req,res) => {
+    res.send('Primer servido'); 
+});
+
+app.get('/datos', (req,res) => {
+    res.send(randoms); 
+});
+
+app.get('/datos/personas', (req,res) => {
+    res.send(randoms.Personas); 
+});
+
+app.get('/datos/personas/:nombre', (req,res) => {
+    const nombre = req.params.nombre;
+    const resultados = randoms.Personas.filter(persona => persona.nombre == nombre);
+    if (resultados.length ===0) {
+        return res.status(404).send(`no hay ${nombre}`); 
+    }
+    res.send(JSON.stringify(resultados));  
+}); 
+
+app.get('/datos/drogas/:nombredroga', (req,res)=>{
+    const nombredroga = req.params.nombredroga; 
+    const resultados = randoms.drogas.filter(droga => droga.nombre == nombredroga);
+    res.send(JSON.stringify(resultados)); 
+}); 
+
+app.post('/', (req,res)=>{
+    let data = req.body;  
+    console.log(data);
+    //randoms.drogas.push(data); 
+    res.send(JSON.stringify(randoms));
+}); 
+
+app.put('/', (req,res)=>{
+    let data = req.body;  
+    console.log(data);
+    res.send(JSON.stringify(randoms));
+}); 
+
+const PUERTO = process.env.PORT || 3000; // en caso de que el servicio entregue puerto 
+app.listen(PUERTO, ()=> {
+    console.log(`servidor rescuchando en ${PUERTO}...`)
+}); 
---
+++ datos.mjs
@@ -0,0 +1,32 @@
+export let randoms = {
+    'Personas' : [
+        {
+            id: 1, 
+            nombre: 'rigoberto', 
+            edad: 25, 
+            poto: 'peludo', 
+            olor: 'citrico'
+        }, 
+
+        {
+            id: 2, 
+            nombre: 'alabama', 
+            edad: 34, 
+            poto: 'plano', 
+            olor: 'sobaco'
+        }
+    ], 
+
+    drogas: [
+        {
+            id: 1, 
+            nombre: 'cafe', 
+            efecto: 'dejardespertar duro'
+        },
+        {
+            id: 2, 
+            nombre: 'exstasis', 
+            efecto: 'no se'
+        }       
+    ]
+}
