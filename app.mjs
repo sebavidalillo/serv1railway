2273bafb49177e89f3381b3b126060d822677db1
Seba Vidal <sebastian.f.vidal@gmail.com>
2023-05-08 11:28:03 -0400

    añadir funciones de get time e instalacion de paquetes childprocess, mysql, moment-timezone

--- app.mjs
+++ app.mjs
@@ -3,11 +3,12 @@ const app = express(); //retorna aplicacion de express.
 import http from 'http'; 
 import {randoms} from './datos.mjs'; 
 import mysql from 'mysql'; 
+import { exec } from "child_process";
+import moment from 'moment-timezone';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
-//port = process.env.PORT || 3000; 
 const port = 3000;
 var database,
 dataTypes = [],
@@ -15,6 +16,13 @@ clients=[],
 dateTimeFormat = 'YYYY-MM-DD HH:mm:ss ZZ',
 timeZone = '-04:00'; 
 //ip = 'my_host'; 
+
+function getTimeZone(){
+    exec("date +%:z", (error, stdout, stderr) => {
+      if(error || stderr){ return ''; }
+      // timeZone = stdout.replace('\n','');
+    });
+  }
 
 database = mysql.createPool({
       connectionLimit:10,
@@ -37,17 +45,17 @@ app.get('/datos', (req,res) => {
 
 app.post('/', function (req, res) {
     //console.log(req.body);
-    let message = req.body; 
-    let rpm = message['can.engine.rpm']; 
-    database.query('insert into messages (id_data, data) value (?,?);',[42,rpm],(error)=>{
-        if(error){
-            throw error;
-        }
-        else{
-            console.log('Inserted in DataBase');
-        }
-      });
-    res.send(200);
+    const message = req.body; 
+    console.log(getDateTime()); 
+    // database.query('insert into messages (id_data, data) value (?,?);',data,(error)=>{
+    //     if(error){
+    //         throw error;
+    //     }
+    //     else{
+    //         console.log('Inserted in DataBase');
+    //     }
+    //   });
+    res.sendStatus(200);
   });
 
 app.put('/', (req,res)=>{
@@ -60,3 +68,22 @@ const PUERTO = process.env.PORT || 3000; // en caso de que el servicio entregue 
 app.listen(PUERTO, ()=> {
     console.log(`servidor rescuchando en ${PUERTO}...`)
 }); 
+
+
+function getDateTime(){
+    try {
+      getTimeZone();
+      return moment.utc().utcOffset(timeZone).format(dateTimeFormat);
+    } catch (error) {
+        console.log('error: '+error+'')
+    }
+  }
+
+function saveData(message){
+    const rpm = message['can.engine.rpm']; 
+    const imei = message['ident']; 
+    const odometer = message['can.vehicle.mileage']; 
+    const fuel = message['can.fuel.volume']; 
+    const mentira = message['mentira']; //si no está definido se queda como undefined
+    //const data = [id_devices_data_types, value, time, ip, port, size, dateEntry];
+}
