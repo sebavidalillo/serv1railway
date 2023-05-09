f7796ce064e5f63fb5168bb3112674327e676801
Seba Vidal <sebastian.f.vidal@gmail.com>
2023-05-09 14:20:52 -0400

    express que tiene funcionando el guardado de datos en BD para post en ruta /

--- app.mjs
+++ app.mjs
@@ -5,6 +5,7 @@ import {randoms} from './datos.mjs';
 import mysql from 'mysql'; 
 import { exec } from "child_process";
 import moment from 'moment-timezone';
+import { time } from 'console';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
@@ -63,7 +64,6 @@ app.listen(PUERTO, ()=> {
     console.log(`servidor rescuchando en ${PUERTO}...`)
 }); 
 
-
 function getDateTime(){
     try {
       getTimeZone();
@@ -74,20 +74,45 @@ function getDateTime(){
   }
 
 function saveData(message){
-  var time = message['timestamp'];
-  var gps = null; 
-  var gps_flag = 0; 
-  //console.log(message['position.latitude']); 
+  var size = 30, //cualquier numero para probar el guardado en base de datos
+  time = message['timestamp'], 
+  value = null, 
+  id_devices_data_types = 1, 
+  dateEntry = moment(new Date(message['timestamp']*1000)).format(dateTimeFormat),
+  [ip, port] = message['peer'].split(':'),
+  gps = null,
+  gps_flag = 0; //indicar que si hay o no informacion del gps en el reporte
+
+  ip = transformIp(ip,1); 
+  port = parseInt(port); 
+
   if (message['position.latitude']!== undefined){
+    id_devices_data_types = 42; 
+    value = 0; 
     gps_flag = 1; 
-    gps = `POINT(${message['position.latitude']}, ${message['position.longitude']})`; 
-    console.log(gps); 
+    //gps = `POINT(${message['position.latitude']} ${message['position.longitude']})`; 
+    gps = [message['position.latitude'], message['position.longitude']]; 
+    try{
+      database.query('insert into md_fleet_devices_data (id_devices_data_types, value, time, gps, ip, port, size, dateEntry) values (?,?,?,POINT(?, ?),?,?,?,?);',[id_devices_data_types, value, time, gps[0], gps[1], ip, port, size, dateEntry],(error)=>{
+        if(error){
+            throw error;
+        }
+        else{
+            console.log('Inserted GPS in DataBase');
+        }
+      });  
+    }
+    catch (error) {
+      console.log('error', error)
+    }
   }
+
   for (let llave in message){
-    var id_devices_data_types = 1; 
-    var value = 0; 
-    var date = null; 
-        if (llave == 'can.engine.rpm'){
+
+    id_devices_data_types = 1,
+    value = 0; 
+
+    if (llave == 'can.engine.rpm'){
       id_devices_data_types = 92; 
       value = message[llave]; 
     } else if (llave == 'can.fuel.volume'){
@@ -96,18 +121,41 @@ function saveData(message){
     } else if (llave == 'can.vehicle.mileage'){
       id_devices_data_types = 83; 
       value = message[llave];
-    } 
-    //console.log(id_devices_data_types, value); 
-   
-    // database.query('insert into md_fleet_devices_data   id_devices_data_types, value, time, ip, port, size, dateEntry)  values ?,?,?,?,?,?,?);',dataToSave,(error)=>{
-    //     if(error){
-    //         throw error;
-    //     }
-    //     else{
-    //         console.log('Inserted in DataBase');
-    //     }
-    //   });
-    // date entry?? = moment(new Date(message[llave]*1000)).format (dateTimeFormat);
-  }
+    };
+
+    if (id_devices_data_types !== 1 && value !== null && time !== null && ip !== null && port !== null && size !== null && dateEntry !== null){
+      try {
+        database.query('insert into md_fleet_devices_data (id_devices_data_types, value, time, ip, port, size, dateEntry)  values (?,?,?,?,?,?,?);',[id_devices_data_types, value, time, ip, port, size, dateEntry],(error)=>{
+             if(error){
+                 throw error;
+             }
+             else{
+                 console.log('Inserted in DataBase');
+             }
+           });
+      }
+      catch (error) {
+        console.log('error:', error)
+      }
+    };
+  };
+
+  
+ 
+
   //console.log(time);
 };
+
+function transformIp(ip,format){
+  try {
+      // 1 = ip to int, 2 = int to ip
+      console.log('transformIp')
+      if(format == 1){
+          return ip.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
+      }else{
+          return ( (ipInt>>>24) +'.' + (ipInt>>16 & 255) +'.' + (ipInt>>8 & 255) +'.' + (ipInt & 255) );
+      }
+  } catch (error) {
+      console.log('error: '+error+'')
+  }
+}
